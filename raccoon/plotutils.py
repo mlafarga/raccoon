1c9241bc0bcde38597925cd1bf09aec12f522b96
Marina Lafarga Magro <marina.lafarga@gmail.com>
2022-08-01 14:53:45 +0100

    Plotting function for lines following a specific colormap

--- raccoon/plotutils.py
+++ raccoon/plotutils.py
@@ -2,7 +2,9 @@
 from __future__ import division
 from __future__ import print_function
 
+import numpy as np
 import matplotlib as mpl
+from matplotlib.collections import LineCollection
 import matplotlib.pyplot as plt
 
 ###############################################################################
@@ -149,3 +151,73 @@ def figout_simple(fig, sv=True, filout='', svext=['png', 'pdf'], sh=False, cl=Tr
     if sh: plt.show()
     if cl: plt.close(fig)
     return
+
+
+# Specific plots
+
+# Plot different lines following a specific cmap
+def multiline(xs, ys, c, ax=None, **kwargs):
+    """Plot lines with different colorings
+
+    Parameters
+    ----------
+    xs : iterable container of x coordinates
+    ys : iterable container of y coordinates
+    c : iterable container of numbers mapped to colormap
+    ax (optional): Axes to plot on.
+    kwargs (optional): passed to LineCollection
+
+    Notes:
+        len(xs) == len(ys) == len(c) is the number of line segments
+        len(xs[i]) == len(ys[i]) is the number of points for each line (indexed by i)
+
+    Returns
+    -------
+    lc : LineCollection instance.
+
+    Source
+    ------
+    https://stackoverflow.com/questions/38208700/matplotlib-plot-lines-with-colors-through-colormap
+    
+    Example (from source above)
+    -------
+    # Plot 30 lines with different Y-intercept (yint)
+
+    from matplotlib.collections import LineCollection
+    import matplotlib.pyplot as plt
+    import numpy as np
+
+    n_lines = 30
+    
+    x = np.arange(100)  # all lines have the same x-axis
+
+    yint = np.arange(0, n_lines*10, 10)  # y-intercept (i.e. offset) for each of the 30 lines. This will be the color values
+    ys = np.array([x + b for b in yint])  # y values of the 30 lines
+    xs = np.array([x for i in range(n_lines)])  # x values of the 30 lines (i.e. x 30 times)
+
+    fig, ax = plt.subplots()
+    lc = multiline(xs, ys, yint, cmap='bwr', lw=2)
+
+    axcb = fig.colorbar(lc)
+    axcb.set_label('Y-intercept')
+    ax.set_title('Line Collection with mapped colors')
+    plt.show()
+    """
+
+    # find axes
+    ax = plt.gca() if ax is None else ax
+
+    # create LineCollection
+    segments = [np.column_stack([x, y]) for x, y in zip(xs, ys)]
+    lc = LineCollection(segments, **kwargs)
+
+    # set coloring of line segments
+    #    Note: I get an error if I pass c as a list here... not sure why.
+    lc.set_array(np.asarray(c))
+
+    # add lines to axes and rescale 
+    #    Note: adding a collection doesn't autoscalee xlim/ylim
+    ax.add_collection(lc)
+    ax.autoscale()
+    return lc
+
